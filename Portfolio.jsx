92756761fb242e8526fb3a18e1b61aa3bcd1be54
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-06-13 19:12:41 -0500

    add react

---
+++ Portfolio.jsx
@@ -0,0 +1,12 @@
+function Portfolio() {
+  return (
+    <div>
+      <h2>Projects: </h2>
+      <a href="https://www.pop-up-app.com">pop^</a>
+      <br></br>
+      <a href="http://165.232.128.76:8080/" target="_blank" >study guide</a>
+    </div>
+  );
+}
+
+ReactDOM.render(<Portfolio />, document.getElementById('app'));
