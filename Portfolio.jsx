d500b78cef4dbc18d047e5696ead360368d3ab55
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-06-23 13:28:18 -0500

    added picture

--- Portfolio.jsx
+++ Portfolio.jsx
@@ -1,10 +1,7 @@
 function Portfolio() {
   return (
     <div>
-      <h2>Projects: </h2>
-      <a href="https://www.pop-up-app.com">pop^</a>
-      <br></br>
-      <a href="http://165.232.128.76:8080/" target="_blank" >study guide</a>
+      <img src="assets/Michael_Regular.jpg" width="40%" height="40%"></img>
     </div>
   );
 }
