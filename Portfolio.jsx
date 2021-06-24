8b2d6c8f17fc3fb249b633d73d0842a68da51206
michaelpaulkunz <MichaelPaulKunz@gmail.com>
2021-06-24 17:33:06 -0500

    underneath pic

--- Portfolio.jsx
+++ Portfolio.jsx
@@ -1,7 +1,16 @@
 function Portfolio() {
+  const state = {
+    pieces: []
+  };
+  let i = 1;
+  while (i < 17) {
+    state.pieces.push(`jigsaw0${i}.png`);
+    i++;
+  }
   return (
     <div>
-      <img src="assets/Michael_Regular.jpg" width="40%" height="40%"></img>
+      <img src={`assets/Michael_Regular.jpg`} width="40%" height="40%"></img>
+      <div style={{verticalAlign: 'center', margin: '25%'}}>{state.pieces.map(piece => <img src={`assets/${piece}`} width="25%" height="25%"></img>)}</div>
     </div>
   );
 }
